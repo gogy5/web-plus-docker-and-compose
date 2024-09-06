bc596ece08171d62a29498b643a7ab5e0372a322
Ilya Prokofiev <ilko199126@gmail.com>
2024-09-07 04:46:34 +0500

    Added frontend and backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,12 +7,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://kupi-5.nomorepartiesco.ru/',
-      'https://kupi-5.nomorepartiesco.ru/',
-    ],
-  });
+  app.enableCors();
 
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
