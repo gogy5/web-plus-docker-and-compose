ce6713fc7958faa905fad047672d2d23e15b5ad6
Ilya Prokofiev <ilko199126@gmail.com>
2024-09-07 04:51:43 +0500

    Added front domains to CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,12 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://kupi-5.nomorepartiesco.ru/',
+      'https://kupi-5.nomorepartiesco.ru/',
+    ],
+  });
 
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
